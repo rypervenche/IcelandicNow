20089d460537c23fd4b459d655ec1e7adbe42293
rypervenche <git@ryper.org>
2024-12-18 22:13:32 -0600

    Test GitHub action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/welcome">
-            Some link might go here?
+            Some link might go here
           </Link>
         </div>
       </div>
